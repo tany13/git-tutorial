cb6c6b84e43447365ec857dfc533c66726292e68
tany13 <tany@lifar.org>
2016-08-28 19:31:18 +0100

    add file

---
+++ jUnit-test/src/test/java/org/lifar/tany/jUnit_test/SuiteTest.java
@@ -0,0 +1,11 @@
+package org.lifar.tany.jUnit_test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class) 
+@Suite.SuiteClasses({AddPencilTest.class, FirstDayAtSchoolTest.class})
+
+public class SuiteTest {
+
+}
